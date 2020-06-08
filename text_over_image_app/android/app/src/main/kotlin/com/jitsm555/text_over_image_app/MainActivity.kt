f2f13b26a601ebee4361e702ec5b02a7d5a5a816
jitsm555 <jitsm555@gmail.com>
2020-06-08 14:25:47 +0530

    initial code

---
+++ text_over_image_app/android/app/src/main/kotlin/com/jitsm555/text_over_image_app/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.jitsm555.text_over_image_app
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
