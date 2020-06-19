c4442de67d269dabc78ca37f691e48005be08b33
jitsm555 <jitsm555@gmail.com>
2020-06-19 19:40:23 +0530

    Done with Tab Bar tricks

---
+++ tab_bar_tricks/android/app/src/main/kotlin/com/jitsm555/tabbartricks/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jitsm555.tabbartricks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ tab_bar_tricks/lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TabBarDemo(),
+    );
+  }
+}
+
+class TabBarDemo extends StatefulWidget {
+  @override
+  _TabBarDemoState createState() => _TabBarDemoState();
+}
+
+class _TabBarDemoState extends State<TabBarDemo>
+    with SingleTickerProviderStateMixin {
+  TabController _controller;
+  int _selectedIndex = 0;
+
+  List<Widget> list = [
+    Tab(icon: Icon(Icons.card_travel)),
+    Tab(icon: Icon(Icons.add_shopping_cart)),
+  ];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // Create TabController for getting the index of current tab
+    _controller = TabController(length: list.length, vsync: this);
+
+    _controller.addListener(() {
+      setState(() {
+        _selectedIndex = _controller.index;
+      });
+      print("Selected Index: " + _controller.index.toString());
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            onTap: (index) {
+              // Should not used it as it only called when tab options are clicked,
+              // not when user swapped
+            },
+            controller: _controller,
+            tabs: list,
+          ),
+          title: Text('Tabs Demo'),
+        ),
+        body: TabBarView(
+          controller: _controller,
+          children: [
+            Center(
+                child: Text(
+              _selectedIndex.toString(),
+              style: TextStyle(fontSize: 40),
+            )),
+            Center(
+                child: Text(
+              _selectedIndex.toString(),
+              style: TextStyle(fontSize: 40),
+            )),
+          ],
+        ),
+      ),
+    );
+  }
+}
